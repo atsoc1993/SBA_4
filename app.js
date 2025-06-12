028405a7db3949c808dd6349cc561e02a3c2f485
atsoc1993 <atsoc1993@gmail.com>
2025-06-12 18:13:37 -0400

    Added accounts.ejs for view engine

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 import express from 'express'
-import { addresses } from './data/FoundationAddresses.js'
+import { addresses, foundationMarketWallets } from './data/FoundationAddresses.js'
 import balances from './data/AddressBalances.js'
 import addressAssets from './data/AddressAssets.js'
-
 let app = express()
 let port = 3000
 
@@ -11,7 +10,12 @@ app.use('/addresses', addresses)
 app.use('/balances', balances)
 app.use('/addressAssets', addressAssets)
 
+app.set('view engine', 'ejs')
+app.set('views', './views')
 
+app.get('/foundation_wallets_view', (req, res) => {
+    res.render('accounts', { foundationMarketWallets } )
+})
 app.listen(port, () => {
     console.log(`Express listening at http://localhost:${port}`)
 })
